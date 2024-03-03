e945a0725124d9360cfcfc7a2f9b741fff9b0aeb
Aziz Alfa <azizalfauzi65@gmail.com>
2024-03-03 10:30:46 +0700

    feat(App-Memo): useCallback in Practice

--- src/App-memo.js
+++ src/App-memo.js
@@ -1,6 +1,7 @@
 import {
   createContext,
   memo,
+  useCallback,
   useContext,
   useEffect,
   useMemo,
@@ -35,9 +36,9 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  const handleAddPost = useCallback(function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
-  }
+  }, []);
 
   function handleClearPosts() {
     setPosts([]);
@@ -79,7 +80,7 @@ function App() {
 
         <Header />
         <Main />
-        <Archive archiveOptions={archiveOptions} />
+        <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} />
         <Footer />
       </section>
     </PostContext.Provider>
@@ -182,8 +183,7 @@ function List() {
   );
 }
 
-const Archive = memo(function Archive({ archiveOptions }) {
-  const { onAddPost } = useContext(PostContext);
+const Archive = memo(function Archive({ archiveOptions, onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
